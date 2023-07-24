a56442016ac614e8a2495e3673158ba3993452f0
riaz9191 <riazahammed9191@gmail.com>
2023-07-24 16:36:43 +0600

    get user

--- index.js
+++ index.js
@@ -52,6 +52,14 @@ async function run() {
       }
     });
 
+     // get user by email
+     app.get('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await usersCollection.findOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
