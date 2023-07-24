8d062d23708d11cf3ec300734f2b2cae8e7dbd4f
riaz9191 <riazahammed9191@gmail.com>
2023-07-24 16:43:43 +0600

    fixed and search updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
@@ -13,9 +14,25 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'Unauthorized access' });
+  } else {
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+      if (error) {
+        return res.status(401).send({ error: true, message: 'Unauthorized access' });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  }
+};
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dzhlcpb.mongodb.net/?retryWrites=true&w=majority`;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv:${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3onslcg.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,12 +48,17 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    
-    const usersCollection = client.db("collageGate").collection("users");
-    const galleryCollection = client.db("collageGate").collection("gallery");
-    const collegesCollection = client.db("collageGate").collection("colleges");
-    const applyCollection = client.db("collageGate").collection("apply");
+    const usersCollection = client.db("collegeGate").collection("users");
+    const galleryCollection = client.db("collegeGate").collection("gallery");
+    const collegesCollection = client.db("collegeGate").collection("colleges");
+    const applyCollection = client.db("collegeGate").collection("apply");
 
+    // handle JWT {todo}
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send(token);
+    });
 
     // insert user
     app.post('/users', async (req, res) => {
@@ -52,59 +74,89 @@ async function run() {
       }
     });
 
-     // get user by email
-     app.get('/users/:email', async (req, res) => {
+    // get user by email
+    app.get('/users/:email', async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
       const result = await usersCollection.findOne(query);
       res.send(result);
     });
-     // get user by id
-     app.get('/edit/:id', async (req, res) => {
+
+    // get user by id
+    app.get('/edit/:id', async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const result = await usersCollection.findOne(filter);
       res.send(result);
     });
-// update user data
-app.put('/users/id/:id', async (req, res) => {
-  const id = req.params.id;
-  const filter = { _id: new ObjectId(id) };
-  const updatedData = req.body;
-  const result = await usersCollection.updateOne(filter, { $set: updatedData });
-  res.send(result);
-});
 
-// get gallery data
-app.get('/gallery', async (req, res) => {
-  const result = await galleryCollection.find().toArray();
-  res.send(result);
-});
+    // update user data
+    app.put('/users/id/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedData = req.body;
+      const result = await usersCollection.updateOne(filter, { $set: updatedData });
+      res.send(result);
+    });
 
-// get colleges data
-app.get('/colleges', async (req, res) => {
-  const result = await collegesCollection.find().toArray();
-  res.send(result);
-});
-// Search colleges based on college name
-app.get('/colleges/search', async (req, res) => {
-  const searchQuery = req.query.query;
-  if (!searchQuery) {
-    return res.status(400).json({ message: 'Search query is required.' });
-  }
+    // get gallery data
+    app.get('/gallery', async (req, res) => {
+      const result = await galleryCollection.find().toArray();
+      res.send(result);
+    });
 
-    const regex = new RegExp(searchQuery, 'i');
-    const filter = { $or: [{ name: regex }, { admission: regex }, { research: { ongoingProjects: regex } }] };
-    const result = await collegesCollection.find(filter).toArray();
-    res.json(result);
-  
-});
- // insert applications
- app.post('/apply', async (req, res) => {
-  const apply = req.body;
-  const result = await applyCollection.insertOne(apply);
-  res.send(result);
-});
+    // get colleges data
+    app.get('/colleges', async (req, res) => {
+      const result = await collegesCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Search colleges based on college name
+    app.get('/colleges/search', async (req, res) => {
+      const searchQuery = req.query.query;
+      if (!searchQuery) {
+        return res.status(400).json({ message: 'Search query is required.' });
+      }
+
+        const regex = new RegExp(searchQuery, 'i');
+        const filter = { $or: [{ name: regex }, { admission: regex }, { research: { ongoingProjects: regex } }] };
+        const result = await collegesCollection.find(filter).toArray();
+        res.json(result);
+      
+    });
+
+    // get specific colleges data
+    app.get('/colleges/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await collegesCollection.findOne(filter);
+      res.send(result);
+    });
+
+    // update college data
+    app.put('/colleges/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedData = req.body;
+      const result = await collegesCollection.updateOne(filter, { $set: updatedData });
+      res.send(result)
+    });
+
+    // insert applications
+    app.post('/apply', async (req, res) => {
+      const apply = req.body;
+      const result = await applyCollection.insertOne(apply);
+      res.send(result);
+    });
+
+    // get specific application data
+    app.get('/apply/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await applyCollection.find(query).toArray(); 
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -117,8 +169,8 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('server is running');
+  res.send('college server is running');
 });
 app.listen(port, () => {
-  console.log(`server is running on port ${port}`);
+  console.log(`college server is running on port ${port}`);
 });
